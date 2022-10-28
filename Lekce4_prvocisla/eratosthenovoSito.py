a0662bdf1bfd2fc5d850a24e5b4a0705e68ed80f
faltovaj <jana.faltova@cern.ch>
2022-10-28 22:29:28 +0200

    small update

--- Lekce4_prvocisla/eratosthenovoSito.py
+++ Lekce4_prvocisla/eratosthenovoSito.py
@@ -11,5 +11,5 @@ sito = [True] * (N+1)
 for i in range(2,N+1):
     if sito[i]:      # pokud cislo v seznamu je
         print(i)     # prvni cislo je prvocislo
-        for j in range(2*i, (N+1), i):    # cyklus pres nasobky daneho cisla
-            sito[j] = False               # nasobky ze seznamu vyjmeme
+        for j in range(i, (N+1), i):    # cyklus pres nasobky daneho cisla
+            sito[j] = False             # nasobky ze seznamu vyjmeme
